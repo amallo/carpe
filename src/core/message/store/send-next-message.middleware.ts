08033cd16c6a56e30658b0a18c5509c2a0c749db
amallo <audie@app2b.io>
2025-09-10 21:10:07 +0200

    refactor(message): optimize message handling middleware for next submitted message dispatch
    - Update the message handling middleware to dispatch only the next submitted message instead of all submitted messages concurrently.
    - Introduce a new selector, `selectNextSubmittedMessage`, to streamline the retrieval of the next message to be sent.
    - Adjust the middleware to listen for both message submission and fulfilled send actions, ensuring efficient message processing.

--- src/core/message/store/send-next-message.middleware.ts
+++ src/core/message/store/send-next-message.middleware.ts
@@ -1,15 +1,30 @@
 import { messageWasSubmitted } from '../usecases/submit-message.usecase';
 import { sendMessage } from '../usecases/send-message.usecase';
-import { selectAllSubmittedMessages } from './message.slice';
 import { startAppListening } from '../../../app/store/middlewares/listener.middleware';
+import { selectNextSubmittedMessage } from './message.slice';
 
 export const listeningSubmittedMessages = () => {
     startAppListening({
         actionCreator: messageWasSubmitted,
         effect: (_, { dispatch, getState }) => {
             const state = getState();
-            const allSubmittedMessages = selectAllSubmittedMessages(state);
-            Promise.all(allSubmittedMessages.map(message => dispatch(sendMessage(message.id))));
+            const nextSubmittedMessage = selectNextSubmittedMessage(state);
+            if (nextSubmittedMessage) {
+                dispatch(sendMessage(nextSubmittedMessage.id));
+            }
+            //Promise.all(allSubmittedMessages.map(message => dispatch(sendMessage(message.id))));
+        },
+    });
+
+    startAppListening({
+        actionCreator: sendMessage.fulfilled,
+        effect: (_, { dispatch, getState }) => {
+            const state = getState();
+            const nextSubmittedMessage = selectNextSubmittedMessage(state);
+            if (nextSubmittedMessage) {
+                dispatch(sendMessage(nextSubmittedMessage.id));
+            }
+            //Promise.all(allSubmittedMessages.map(message => dispatch(sendMessage(message.id))));
         },
     });
 };
